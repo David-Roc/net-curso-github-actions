cd75835706fb1f073ba05385c238688d33699d62
David Rodriguez Canto <david.leo.rodriguez.c@gmail.com>
2025-10-28 17:24:29 -0600

    se agrego el yml de contextos

--- GitHubActionsCourse/obj/Debug/net9.0/GitHubActionsCourse.AssemblyInfo.cs
+++ GitHubActionsCourse/obj/Debug/net9.0/GitHubActionsCourse.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GitHubActionsCourse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9cee9c8198ff31951bd427da89717f5ea8b0e1c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e488086e71552dc908a9d21dc214edf6a69eb52")]
 [assembly: System.Reflection.AssemblyProductAttribute("GitHubActionsCourse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GitHubActionsCourse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
